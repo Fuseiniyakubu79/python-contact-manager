fd513fcdf1c7b0a4ed4b3447db4eaba60c691bd1
TheGamingHideout <owner@thegaminghideout.com>
2015-09-28 10:22:52 -0500

    Update contact_manager.py

--- contact_manager.py
+++ contact_manager.py
@@ -1,150 +1,174 @@
-def build_db(path):
-    db = {}
-    debug_dict = {}
-    with open(path) as f:
-        for line in f:
-            category, value = map(str.strip, line.split(":"))
-            if category == "CONTACT":
-                cur_contact = value
-                db[value] = {}
-            else:
-                db.get(cur_contact, {})[category] = value
-    # builds a dictionary of dictionaries that looks like:
-    # # {"YOU": {"First Name": "FELIX", "Last Name": "MARTIN", ...}, ...}
-    print(db)
-    return db
+import time
+#define the database building API
+def build_db(path, mode=0):
+    if mode == 0:
+        
+        db = {}
+        #while opening contacts.txt as f
+        with open(path) as f:
+            for line in f:
+                #strip the file and split each line from the 'CATEGORY' and the 'VALUE'
+                #e.g. split 'CONTACT: YOU' to 'CONTACT', 'YOU'
+                category, value = map(str.strip, line.split(":"))
+                #if the stripped line is the CONTACT line
+                if category == "CONTACT":
+                    #set the current contact to start a new dictionary with the value of the contact
+                    cur_contact = value
+                    db[value] = {}
+                #if the stripped line is the data within the CONTACT line
+                else:
+                    #set the previously set dictionarys data with another dictionary
+                    db.get(cur_contact, {})[category] = value
+        #return the database when its called
+        return db
+    #unless debug mode is set to 1
     
+    else:
+        #do everything as before, except print data for debugging
+        db = {}
+        with open(path) as f:
+            for line in f:
+                category, value = map(str.strip, line.split(":"))
+                mapDebug = map(str.strip, line.split(":"))
+                print("Printing mapDebug\n")
+                print(mapDebug)
+                print("Printing category, value\n") 
+                print(category, value)
+                if category == "CONTACT":
+                    cur_contact = value
+                    db[value] = {}
+                    print("Printing db[value]\n")
+                    print(db[value])
+                else:
+                    print("Printing db.get(cur_contact, {})[category]\n")
+                    print(db.get(cur_contact, {})[category])
+                    db.get(cur_contact, {})[category] = value
+        print(db)
+        return db        
+#finally, build the database off of the local contacts.txt file
 db = build_db("contacts.txt")
+#Application
+
 class App:
+    #initialize the Application
     def __init__(self):
-        
-        print("Contact Manager - Felix Martin V2.0")
+        #print info
+        print("Contact Manager - Felix Martin V2.8.1")
         print("LOADING . . .")
+        print("LOADED!")
+        self.contact_manager(db)
+
+    def add_contact_to_db(self, db, contact_name, contact_fname, contact_lname, contact_number, contact_address):
+        #set the contact_info to be empty
+        contact_info = {}
+        #also check to see if the contact name already exists
+        contact_info_cmd = db.get(contact_name)
+
+        #if the contact_info_cmd is not None, or has existing data
+        if contact_info_cmd is not None:
+            raise KeyError("Contact already exists: {}".format(contact_name))
+        #set uppercase data equal to user input
+        contact_info['contact'] = contact_name.upper()
+        contact_info['First Name'] = contact_fname.upper()
+        contact_info['Last Name'] = contact_lname.upper()
+        contact_info['Number'] = contact_number.upper()
+        contact_info['Address'] = contact_address.upper()
         
+        #open up the file
+        with open('contacts.txt', 'a+') as f:
+            #write the data
+            f.write("\nCONTACT: " + contact_info['contact'])
+            f.write("\n    First Name: " + contact_info['First Name'])
+            f.write("\n    Last Name: " + contact_info['Last Name'])
+            f.write("\n    Number: " + contact_info['Number'])
+            f.write("\n    Address: " + contact_info['Address'])
+            #rebuild the db
+        db = build_db("contacts.txt")
+            
         self.contact_manager(db)
         
-        print("LOADED!")
-    #debugging for a new function
-    def add_contact_to_db_data(self, db):
-        
-        formatting = """\
-        CONTACT: {contact}
-            First Name: {First Name}
-            Last Name: {Last Name}
-            Number: {Number}
-            Address: {Address}
-        """      
-        
-        refname = input("What is the contacts reference name?\n")
-        fname = input("What is the contacts first name?\n")
-        lname = input("What is the contacts last name?\n")
-        number = input("What is the contacts (cell/main/home/etc) number?\n")
-        address = input("What is the contacts address?\n")
-        
-        contact_name = refname
-        contact_fname = fname
-        contact_lname = lname
-        contact_number = number
-        contact_address = address
-        
-        contact_info = db.get(contact_name)
-        
-        if contact_info is not None:
-            raise KeyError("Contact already exists: {}".format(contact_name))
-
-        
-        self.add_contact_to_db(db, contact_name, contact_fname, contact_lname, contact_number, contact_address)
-    #more debugging
-    def add_contact_to_db(self, db, contact_name, contact_fname, contact_lname, contact_number, contact_address):
-        
-        formatting = """\
-        CONTACT: {contact}
-            First Name: {firstname}
-            Last Name: {lastname}
-            Number: {number}
-            Address: {address}
-        """
-        contact_info = db.append(contact_name)
-        
-        if contact_info is not None:
-            raise KeyError("Contact already exists: {}".format(contact_name))
-        print(contact_info['contact'])
-        #contact_info['contact'] = contact_name.upper()
-        #contact_info['firstname'] = contact_fname.upper()
-        #contact_info['lastname'] = contact_lname.upper()
-        #contact_info['number'] = contact_number.upper()
-        #contact_info['address'] = contact_address.upper()
-        
-        print(formatting.format(**contact_info))
-        
     def read_contact(self, db, contact_name):
+        #follow format
         formatting = """\
-        CONTACT: {contact}
-            First Name: {First Name}
-            Last Name: {Last Name}
-            Number: {Number}
-            Address: {Address}
-        """
+CONTACT: {contact}
+    First Name: {First Name}
+    Last Name: {Last Name}
+    Number: {Number}
+    Address: {Address}
+"""
+        #grab user input
         contact_name = contact_name.upper()
         contact_info = db.get(contact_name)
-        
+        #if userinput isnt a valid a contact        
         if contact_info is None:
             raise KeyError("No such contact: {}".format(contact_name))
+        #set the data equal to the user input
         contact_info['contact'] = contact_name
-        return formatting.format(**contact_info)
+        #finally, print the found contact and its values following the format
+        print(formatting.format(**contact_info))
+        
+        self.contact_manager(db)
     
     def write_db_to_file(self, db, out_path):
         with open(out_path, 'w') as outf:
             for contact_name in db:
-                contact_name = contact_name.upper()
-                outf.write(self.read_contact(db, contact_name))
-    
+                outf.write(self.read_contact(db, contact_name.upper()))
+        
+        self.contact_manager(db)        
+                
     def remove_user_from_db(self, db, contact_name):
+        #try to delete specificed contact
         try:
             del db[contact_name.upper()]
-        except KeyError:
+        #but if there is no such contact
+        except KeyError or contact_name.upper() is "dev":
             raise KeyError("No such contact: {}".format(contact_name))
-    
-    def debug_function(self, db, contact_name):
-        formatting = """\
-        CONTACT: {contact}
-            First Name: {First Name}
-            Last Name: {Last Name}
-            Number: {Number}
-            Address: {Address}
-        """        
-        print(db)
         self.contact_manager(db)
-        
-        
+    
     def contact_manager(self, db):
 
+        #init the menu
         print("Type a command, help or quit to exit the program.")
         cmd = input("")
+        #if the cmd is help
         if cmd.upper() == "HELP":
             print("Available commands are: addContact, delContact, writeFile, readFile, help, quit")
             
             self.contact_manager(db)
-            
+        #or if the cmd is addcontact
         elif cmd.upper() == "ADDCONTACT":
             
-            self.add_contact_to_db_data(db)
-        
+            self.add_contact_to_db(db, input("What is the contacts reference name?\n").upper(), input("What is the contacts first name?\n").upper(), input("What is the contacts last name?\n").upper(), input("What is the contacts (cell/main/home/etc) number?\n").upper(), input("What is the contacts address?\n").upper())
+        #or if the cmd is writefile...
         elif cmd.upper() == "WRITEFILE":
             
             self.write_db_to_file(db, "contacts.txt")
-        
+        #... etc etc etc
         elif cmd.upper() == "READFILE":
             
-            self.read_contact(db, input("What contact shall you search?"))
+            self.read_contact(db, input("What contact shall you search?\n").upper())
         
         elif cmd.upper() == "DELCONTACT":
             
-            self.remove_user_from_db(db, input("What contact shall you delete?").upper())
+            self.remove_user_from_db(db, input("What contact shall you delete?\n"))
+        #if the cmd is quit
+        elif cmd.upper() == "QUIT":
+            #thank the user for using my program
+            print("Thank you for using Contact Manager.")
+            #delay2 seconds
+            time.sleep(2)
+            quit()
+
+        #if the user activates the secret command
         elif cmd.upper() == "DEBUG()":
             
             print("Activating DEBUG Function.\n")
-            self.debug_function(db, "")
+            print(db)
+            self.contact_manager(db)
+        #finally, if it is none of these
+        else:
+            print("Unknown command.")
+            self.contact_manager(db)
             
-            
+#run the app    
 App()
